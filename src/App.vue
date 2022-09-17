34844c5902ee1c313662fcf5259291a8c9b73fd5
JAMIL8051 <jamilreza@iut-dhaka.edu>
2022-09-17 01:36:20 -0400

    modifcation of hello-world porject

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Digital photo" src="./assets/Jamil.jpg">
+    <HelloWorld msg="Welcome to Jamil Chowdhury's web-portal"/>
   </div>
 </template>
 
